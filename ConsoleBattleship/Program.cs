b6ad78f82510e47fde77e33c0ba260d7613eaa89
Fynis96 <C.hearth96@gmail.com>
2023-05-09 19:00:43 -0400

    Start game loop, created a DisplayShotGrid method

--- ConsoleBattleship/Program.cs
+++ ConsoleBattleship/Program.cs
@@ -8,10 +8,62 @@ namespace ConsoleBattleship
         static void Main(string[] args)
         {
             WelcomeMessage();
-            PlayerInfoModel player1 = CreatePlayer("Player 1");
-            PlayerInfoModel player2 = CreatePlayer("Player 2");
+
+            PlayerInfoModel activePlayer = CreatePlayer("Player 1");
+            PlayerInfoModel opponent = CreatePlayer("Player 2");
+            PlayerInfoModel winner = null;
+
+            do
+            {
+                // Display grid from activePlayer on where they fired
+                DisplayShotGrid(activePlayer);
+
+                // Ask activePlayer for a shot
+
+                // Determine if it is a valid shot
+
+                // Determine shot results
+
+                // Determine if game is over
+
+                // If over, set activePlayer as the winner
+
+                //else, swap positions (activePlayer to opponent)
+
+            } while (winner == null);
 
             Console.ReadLine();
+        }
+
+        private static void DisplayShotGrid(PlayerInfoModel activePlayer)
+        {
+            string currentRow = activePlayer.ShotGrid[0].SpotLetter;
+
+            foreach (var gridSpot in activePlayer.ShotGrid)
+            {
+                if (gridSpot.SpotLetter != currentRow)
+                {
+                    Console.WriteLine();
+                    currentRow = gridSpot.SpotLetter;
+                }
+
+                if (gridSpot.Status == GridSpotStatus.Empty)
+                {
+                    Console.Write($" {gridSpot.SpotLetter}{gridSpot.SpotNumber} ");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Hit)
+                {
+                    Console.Write(" X ");
+                }
+                else if (gridSpot.Status == GridSpotStatus.Miss)
+                {
+                    Console.Write(" O ");
+                }
+                else
+                {
+                    Console.Write(" ? ");
+                }
+            }
         }
 
         private static void WelcomeMessage()
@@ -33,16 +85,6 @@ namespace ConsoleBattleship
                     Console.WriteLine("That was not a valid location, Please try again.");
                 }
             } while (model.ShipLocations.Count < 5);
-        }
-
-        private static void DisplayScore()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void PrintGrid()
-        {
-            throw new NotImplementedException();
         }
 
         private static string AskForUsersName()
