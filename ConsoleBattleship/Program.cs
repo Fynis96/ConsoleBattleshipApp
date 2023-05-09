fdb6ca13ea547901e8f222741ba33f2bc579104e
Fynis96 <C.hearth96@gmail.com>
2023-05-08 20:54:10 -0400

    Add project files.

---
+++ ConsoleBattleship/Program.cs
@@ -0,0 +1,40 @@
+﻿using BattleshipClassLibrary.Models;
+
+namespace ConsoleBattleship
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+
+            Console.ReadLine();
+        }
+
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to Battleship Lite");
+            Console.WriteLine("created by Christian Foster\n");
+        }
+        private static void GetShipPlacement()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void DisplayScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void PrintGrid()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string AskForName()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
